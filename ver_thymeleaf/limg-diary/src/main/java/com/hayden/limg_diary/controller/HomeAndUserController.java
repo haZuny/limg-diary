ca1b5d38453f52e9f89fcb85c0d213c700b5b0a0
haZuny <gkwns5791@naver.com>
2024-06-11 14:13:54 +0900

    [feat] HomeAndUserController 파일 생성

---
+++ ver_thymeleaf/limg-diary/src/main/java/com/hayden/limg_diary/controller/HomeAndUserController.java
@@ -0,0 +1,14 @@
+package com.hayden.limg_diary.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeAndUserController {
+
+    @GetMapping("/home")
+    public void getHome(){
+        
+    }
+}
