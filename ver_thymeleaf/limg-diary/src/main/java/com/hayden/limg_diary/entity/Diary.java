2e6bbef88b26a6455dd88239c2e8925d03279fd7
haZuny <gkwns5791@naver.com>
2024-06-15 21:17:11 +0900

    [chore] date 필드의 타입 변경
    - jpa에서 날짜를 저장할때 java.sql 타입을 전환을 못함
      - date 필드의 타입을 java.sql -> java.util로 전환

--- ver_thymeleaf/limg-diary/src/main/java/com/hayden/limg_diary/entity/Diary.java
+++ ver_thymeleaf/limg-diary/src/main/java/com/hayden/limg_diary/entity/Diary.java
@@ -8,7 +8,8 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.sql.Date;
+import java.util.Date;
+
 
 @Entity
 @Getter
