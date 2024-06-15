be80e76a1c52aa118fb4928852186902e2e02291
haZuny <gkwns5791@naver.com>
2024-06-15 20:42:58 +0900

    [fix] 다이어리의 디폴트 이미지 적용 안되는 문제 해결
    - defaultImgPath 변수 제거 및 관련 메소드 제거
      - 서비스단에서 기본 이미지 경로 설정하도록 변경

--- ver_thymeleaf/limg-diary/src/main/java/com/hayden/limg_diary/entity/Diary.java
+++ ver_thymeleaf/limg-diary/src/main/java/com/hayden/limg_diary/entity/Diary.java
@@ -3,13 +3,17 @@ package com.hayden.limg_diary.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.sql.Date;
 
 @Entity
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class Diary {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
