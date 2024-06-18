ab633ad68d1027286a24a9158fc6f179ac304a1b
haZuny <gkwns5791@naver.com>
2024-06-18 23:05:38 +0900

    [test] diaryRepository - 오늘 날짜 일기 조회 테스트 코드 작성

---
+++ ver_thymeleaf/limg-diary/src/test/java/com/hayden/limg_diary/test/repository/DiaryRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.hayden.limg_diary.test.repository;
+
+import com.hayden.limg_diary.entity.Diary;
+import com.hayden.limg_diary.entity.User;
+import com.hayden.limg_diary.repository.DiaryRepository;
+import com.hayden.limg_diary.test.DataGenerater;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@SpringBootTest
+public class DiaryRepositoryTest {
+
+    @Autowired
+    DiaryRepository diaryRepository;
+
+    @Autowired
+    DataGenerater dataGenerater;
+
+    @Test
+    @Transactional
+    public void findByUserAndDateBetweenTest(){
+        // Given
+        String username = "asbvaksdbgaskjdhgishf";
+        String userpassword = "asbvaksdbgaskjdhgishf";
+        Optional<User> user = dataGenerater.createUser(username, userpassword);
+        String content = "안녕하세요";
+        String feels = "3";
+        dataGenerater.createDiary(content, feels, user.get().getUserid());
+
+        // When
+        LocalDateTime date1 = LocalDateTime.now().withHour(0).withMinute(0).withMinute(0);
+        LocalDateTime date2 = LocalDateTime.now().withHour(23).withMinute(59).withMinute(59);
+        Diary diary = diaryRepository.findByUseridAndDateBetween(user.get(), date1, date2);
+
+        // Then
+        Assertions.assertNotNull(diary);
+        Assertions.assertEquals(content, diary.getContent());
+    }
+
+    @Test
+    @Transactional
+    public void findByUserAndDateTest(){
+        // Given
+        String username = "asbvaksdbgaskjdhgishf";
+        String userpassword = "asbvaksdbgaskjdhgishf";
+        Optional<User> user = dataGenerater.createUser(username, userpassword);
+        String content = "안녕하세요";
+        String feels = "3";
+        dataGenerater.createDiary(content, feels, user.get().getUserid());
+
+        // When
+        Diary diary = diaryRepository.findByUserAndDate(user.get(), LocalDate.now());
+
+        // Then
+        Assertions.assertNotNull(diary);
+        Assertions.assertEquals(content, diary.getContent());
+    }
+}
