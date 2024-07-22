4003258027c4b111bbf6628660619bab8b58a670
haZuny <gkwns5791@naver.com>
2024-07-22 13:55:34 +0900

    [chore] 서버 프로젝트 생성

---
+++ limg-diary-server/src/main/java/com/hayden/limg_diary/LimgDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.hayden.limg_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimgDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimgDiaryApplication.class, args);
+	}
+
+}
