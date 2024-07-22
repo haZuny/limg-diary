8731c51df267a8e47744e52b1c7845179d14bbe9
haZuny <gkwns5791@naver.com>
2024-07-22 14:43:11 +0900

    [feat] users entity 추가
    - basetimeentity 추가
    - main application.java에 auditing 추가
    - user entity 추가

---
+++ limg-diary-server/src/main/java/com/hayden/limg_diary/entity/BaseTimeEntity.java
@@ -0,0 +1,29 @@
+package com.hayden.limg_diary.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@MappedSuperclass   // BaseTimeEntity를 상속한 클래스는, BaseTimeEntity의 필드를 칼럼으로 인식
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private int id;
+
+    @CreatedDate
+    @Column(name = "CREATED_DATE")
+    private Date createdData;
+
+    @LastModifiedDate
+    @Column(name = "UPDATED_DATE")
+    private Date updatedData;
+}
