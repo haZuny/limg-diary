58db0cc663220a472660a23658ec8c46f78f63c3
haZuny <gkwns5791@naver.com>
2024-08-02 16:37:08 +0900

    [feat] deepl, karlo api 관련 코드 추가

---
+++ limg-diary-server/src/main/java/com/hayden/limg_diary/api/deepl/dto/DeeplTransitionForm.java
@@ -0,0 +1,11 @@
+package com.hayden.limg_diary.api.deepl.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeeplTransitionForm {
+    String detected_source_language;
+    String text;
+}
---
+++ limg-diary-server/src/main/java/com/hayden/limg_diary/api/karlo/dto/KarloResponseForm.java
@@ -0,0 +1,15 @@
+package com.hayden.limg_diary.api.karlo.dto;
+
+import com.hayden.limg_diary.api.api_dto.KarloImageForm;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class KarloResponseForm {
+    String id;
+    String model_version;
+    List<KarloImageForm> images;
+}
